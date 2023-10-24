185f135f9717b8736aec40ba849613fa9267f6c0
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-10-24 08:40:21 -0300

    Demonstração de como deletar dados

---
+++ 05 - Demonstração de como deletar dados/README.md
@@ -0,0 +1,7 @@
+## Demonstração de como deletar dados
+
+###### Lista de controle:
+
+- Criar DBIntegrityException
+
+- Tratar a exceção de integridade referencial
---
+++ 05 - Demonstração de como deletar dados/src/application/Program.java
@@ -0,0 +1,36 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import db.DB;
+import db.DBIntegrityException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		
+		try {
+			connection = DB.getConnection();
+			
+			preparedStatement = connection.prepareStatement(
+					"DELETE FROM department "
+					+ "WHERE Id = ?");
+			
+			preparedStatement.setInt(1, 2);
+			
+			int rowsAffected = preparedStatement.executeUpdate();
+			
+			System.out.println("Done! Rows affected: " + rowsAffected);
+		} catch (SQLException e) {
+			throw new DBIntegrityException(e.getMessage());
+		} finally {
+			DB.closeStatement(preparedStatement);
+			DB.closeConnection();
+		}
+	}
+
+}
